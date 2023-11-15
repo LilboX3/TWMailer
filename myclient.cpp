426cf81939d98bca83d1ac08339fa64eb6bf56c9
Toukage <127383796+Toukage@users.noreply.github.com>
2023-11-15 11:38:59 +0100

    Update myclient.cpp

--- myclient.cpp
+++ myclient.cpp
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <iostream>
 #include <sstream>
+#include <ldap.h>
 using namespace std;
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -22,6 +23,18 @@ int readCommand(int socket);
 int delCommand(int socket);
 int specificMessage(int socket);
 
+///////////////////////////////////////////////////////////////////////////////
+const char *LDAP_SERVER = "ldap.technikum.wien.at";
+const int LDAP_PORT = 389;
+const char *LDAP_SEARCH_BASE = "dc=technikum-wien,dc=at";
+const int MAX_LOGIN_ATTEMPTS = 3;
+const int BLACKLIST_DURATION = 60;
+int loginAttempts = 0;
+int authUser(int socket);
+int LdapAuth(const string& username, const string& password);
+
+///////////////////////////////////////////////////////////////////////////////
+
 int main(int argc, char **argv)
 {
    int create_socket;
@@ -31,14 +44,14 @@ int main(int argc, char **argv)
    int isQuit = 0;
 
    if(argc != 3){
-      perror("Usage: ./twmailer-client <ip> <port>");
+      cerr << "Usage: ./twmailer-client <ip> <port>";
       return EXIT_FAILURE;
    }
 
    std::istringstream iss(argv[2]);
    int port;
    if(!(iss >> port)){
-      perror("Invalid port - not a number");
+      cerr << "Invalid port - not a number";
       return EXIT_FAILURE;
    }
    ////////////////////////////////////////////////////////////////////////////
@@ -77,7 +90,7 @@ int main(int argc, char **argv)
 
    // ignore return value of printf
    printf("Connection with server (%s) established\n",
-          inet_ntoa(address.sin_addr));
+         inet_ntoa(address.sin_addr));
 
    ////////////////////////////////////////////////////////////////////////////
    // RECEIVE DATA
@@ -96,6 +109,15 @@ int main(int argc, char **argv)
       buffer[size] = '\0';
       printf("%s", buffer); // ignore error
    }
+   memset(buffer, 0, BUF);
+
+   // Perform user authentication
+   if (authUser(create_socket) == -1) {
+   // Authentication failed
+      perror("Authentication failed");
+      return EXIT_FAILURE;
+   }
+
 
    do
    {
@@ -103,24 +125,24 @@ int main(int argc, char **argv)
       string command;
       getline(cin, command);
       if(command=="SEND"){
-        if(sendCommand(create_socket) == -1){
+         if(sendCommand(create_socket) == -1){
             continue;
-        }
+         }
       }
       else if(command=="LIST"){
          if(listCommand(create_socket) == -1){
             continue;
-        }
+         }
       }
       else if(command=="READ"){
          if(readCommand(create_socket) == -1){
             continue;
-        }
+         }
       }
       else if(command=="DEL"){
          if(delCommand(create_socket) == -1){
             continue;
-        }
+         }
       }
       else if(command=="QUIT"){
          isQuit = 1;
@@ -147,7 +169,15 @@ int main(int argc, char **argv)
          // solution 2: add an infrastructure component for messaging (broker)
          //
          if(!isQuit){
-            size = recv(create_socket, buffer, BUF - 1, 0);
+
+            while(true){
+               size = recv(create_socket, buffer, BUF - 1, 0);
+               //cout << "currently in buffer: --> "<<buffer << "-->END"<<endl;
+               /*if(strncmp("OK", buffer, 3)==0||strncmp("ERR", buffer, 4)==0){
+                  printf("<< %s\n", buffer);
+                  break;
+               }*/
+
             if (size == -1)
             {
                perror("recv error");
@@ -161,12 +191,22 @@ int main(int argc, char **argv)
             else
             {
                buffer[size] = '\0';
-               printf("<< %s\n", buffer); // ignore error
-               if (strcmp("OK", buffer) != 0)
-               {
-                  fprintf(stderr, "<< Server error occured, abort\n");
+               printf("%s\n", buffer);
+
+               //Buffer doesnt receive line by line: check if OK or ERR is contained anywhere in there.
+               char *output = NULL;
+               output = strstr (buffer,"<< OK");
+               if(output) {
+                  //printf("<< OK\n");
                   break;
                }
+               output = strstr (buffer,"<< ERR");
+               if(output) {
+                  //printf("<< ERR\n");
+                  break;
+               }
+            }
+            memset(buffer, 0, BUF);
             }
          }
    } while (!isQuit);
@@ -278,7 +318,7 @@ int readCommand(int socket){
 }
 
 int delCommand(int socket){
-   if ((send(socket, "DELETE", 4, 0)) == -1) 
+   if ((send(socket, "DEL", 4, 0)) == -1) 
       {
          perror("send error");
          return -1;
@@ -314,3 +354,91 @@ int specificMessage(int socket){
 }
 
 // ./twmailer-client 127.0.0.1 1
+
+
+int LdapAuth(const string &username, const string &password){
+   LDAP *ldap;
+   int ldapVersion = LDAP_VERSION3;
+   int ldapResult;
+
+   if ((ldap = ldap_init(LDAP_SERVER, LDAP_PORT)) == NULL){
+      perror("LDAP initialization error");
+      return -1;
+   }
+
+   ldap_set_option(ldap, LDAP_OPT_PROTOCOL_VERSION, &ldapVersion);
+
+   stringstream bindDN;
+   bindDN << "uid=" << username << "," << LDAP_SEARCH_BASE;
+   const char *ldapBindDN = bindDN.str().c_str();
+
+   ldapResult = ldap_simple_bind_s(ldap, ldapBindDN, password.c_str());
+   if (ldapResult != LDAP_SUCCESS) {
+      fprintf(stderr, "LDAP bind failed: %s\n", ldap_err2string(ldapResult));
+      return -1;
+   }
+
+   printf("LDAP bind successful.\n");
+
+   // Close the LDAP connection
+   ldap_unbind(ldap);
+
+   return 0;
+}
+
+int authUser(int socket) {
+   char buffer[BUF];
+
+   // Read username from the console
+   std::string username;
+   std::cout << "Username: ";
+   getline(std::cin, username);
+
+   // Read password from the console (hide input)
+   std::string password;
+   std::cout << "Password: ";
+   system("stty -echo");
+   getline(std::cin, password);
+   system("stty echo");
+   std::cout << std::endl;
+
+   // Send the LOGIN command to the server
+   if (send(socket, "LOGIN", 5, 0) == -1) {
+      perror("send error");
+      return -1;
+   }
+
+   // Send the username to the server
+   if (send(socket, username.c_str(), username.size(), 0) == -1) {
+      perror("send error");
+      return -1;
+   }
+
+   // Send the password to the server
+   if (send(socket, password.c_str(), password.size(), 0) == -1) {
+      perror("send error");
+      return -1;
+   }
+
+   // Receive the authentication result from the server
+   if (recv(socket, buffer, BUF - 1, 0) == -1) {
+      perror("recv error");
+      return -1;
+   }
+
+   buffer[size] = '\0';
+
+   if (strcmp("OK", buffer) == 0) {
+      std::cout << "Authentication successful.\n";
+      loginAttempts = 0; // Reset login attempts on successful login
+      return 0;
+   } else {
+      std::cout << "Authentication failed. Please try again.\n";
+      loginAttempts++;
+      if (loginAttempts >= MAX_LOGIN_ATTEMPTS) {
+         std::cout << "Too many login attempts. Exiting.\n";
+         exit(EXIT_FAILURE);
+      }
+      return -1;
+   }
+}
